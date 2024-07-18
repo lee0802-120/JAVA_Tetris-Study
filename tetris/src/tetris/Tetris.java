6f61cc90c79e4e61a03722fff9f1ef886b8fec68
Junsuk-Desktop <Junsuk-Desktop@59.6.91.11>
2024-07-18 22:18:50 +0900

    Use class.

--- tetris/src/tetris/Tetris.java
+++ tetris/src/tetris/Tetris.java
@@ -3,39 +3,58 @@ package tetris;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.imageio.IIOImage;
+import javax.imageio.ImageIO;
 import javax.swing.*;
 
 public class Tetris extends JFrame {
 	public static final int M = 20;
 	public static final int N = 10;
 	private ImageIcon mainBackGroundImage = new ImageIcon("img/background.png");
-	private ImageIcon fieldImage = new ImageIcon("img/tiles.png");
+	private BufferedImage fieldImage;
 	private Image imageMainBackGround = mainBackGroundImage.getImage();
 	private BufferedImage combinedImage;
 	private int[][] field = new int[M][N];
 	private int[][] tiles = new int[][] {
-		// [7][4]
-			{1,3,5,7}, // I
-			{2,4,5,7}, // Z
-			{3,5,4,6}, // S
-			{3,5,4,7}, // T
-			{2,3,5,7}, // L
-			{3,5,7,6}, // J
-			{2,3,4,5}  // O
+			// [7][4]
+			{ 1, 3, 5, 7 }, // I
+			{ 2, 4, 5, 7 }, // Z
+			{ 3, 5, 4, 6 }, // S
+			{ 3, 5, 4, 7 }, // T
+			{ 2, 3, 5, 7 }, // L
+			{ 3, 5, 7, 6 }, // J
+			{ 2, 3, 4, 5 } // O
 	};
-	
+
 	public Tetris() {
 		super("Tetris Game!");
 		setSize(400, 600);
 		setLocationRelativeTo(null);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+		try {
+			fieldImage = ImageIO.read(new File("img/tiles.png"));
+
+			List<BufferedImage> croppedImages = new ArrayList<>();
+
+			croppedImages.add(TilesImage.cropImage(fieldImage, 0, 0, 17, 17));
+
+			combinedImage = TilesImage.combineImages(croppedImages, 2, 2, 100, 100);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
 		MyPanel mainPanel = new MyPanel();
 		setContentPane(mainPanel);
-		
+
 		setVisible(true);
 	}
-	
+
 	class MyPanel extends JPanel {
 		@Override
 		public void paintComponent(Graphics g) {
@@ -43,6 +62,7 @@ public class Tetris extends JFrame {
 			int panelWidth = getWidth();
 			int panelHeight = getHeight();
 			g.drawImage(imageMainBackGround, 0, 0, panelWidth, panelHeight, this);
+			g.drawImage(combinedImage, 17, 17, null);
 		}
 	}
 }
--- tetris/src/tetris/TilesImage.java
+++ tetris/src/tetris/TilesImage.java
@@ -2,16 +2,37 @@ package tetris;
 
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
+
 import java.util.List;
 
 public class TilesImage {
+	
+	/**
+     * 이미지를 자릅니다.
+     * @param src 원본 이미지
+     * @param x 자르기 시작 X 좌표
+     * @param y 자르기 시작 Y 좌표
+     * @param width 자를 너비
+     * @param height 자를 높이
+     * @return 자른 이미지
+     */
 	
 	public static BufferedImage cropImage(BufferedImage src, int x, int y, int width, int height) {
 		BufferedImage dest = src.getSubimage(x, y, width, height);
 		return dest;
 	}
 	
-	public static BufferedImage combineImage(List<BufferedImage> images, int rows, int cols, int width, int height) {
+	/**
+     * 여러 이미지를 합칩니다.
+     * @param images 합칠 이미지 목록
+     * @param rows 합칠 이미지의 행 수
+     * @param cols 합칠 이미지의 열 수
+     * @param width 각 이미지의 너비
+     * @param height 각 이미지의 높이
+     * @return 합쳐진 이미지
+     */
+	
+	public static BufferedImage combineImages(List<BufferedImage> images, int rows, int cols, int width, int height) {
 		BufferedImage combined = new BufferedImage(width * cols, height * rows, BufferedImage.TYPE_INT_ARGB);
 		Graphics2D g2d = combined.createGraphics();
 		
@@ -24,5 +45,6 @@ public class TilesImage {
 		g2d.dispose();
 		return combined;
 	}
+	
 
 }
